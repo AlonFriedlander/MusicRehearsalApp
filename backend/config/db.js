560ab6f909767257a6bf1f3663244637704fa899
alonf <alonf1536@gmail.com>
2024-11-06 14:15:29 +0200

    try

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log('MONGO_URI:', process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
