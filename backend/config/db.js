8deccb8df2125042e942593afe42a00545725201
alonf <alonf1536@gmail.com>
2024-11-06 13:56:06 +0200

    change db

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import colors from 'colors';
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect('mongodb+srv://alonf1536:Barcelona1841!@cluster0.ho99g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
